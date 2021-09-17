1165603e047caa5223708256b8d0f80c4f6d049f
Angel Kirkov <angelnkirkov@gmail.com>
2021-09-17 12:55:15 +0300

    edit2

--- src/js/app.js
+++ src/js/app.js
@@ -9,5 +9,6 @@ window.addEventListener("DOMContentLoaded", () => {
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+  R.pluck("class", arrayToPluck);
   const articles = document.querySelectorAll("article");
 });
